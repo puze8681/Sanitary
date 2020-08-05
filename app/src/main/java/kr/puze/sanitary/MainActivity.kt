0f453980227d586d6811240a342fcadd11c72608
puze8681 <puze8681@gmail.com>
2020-08-06 03:59:26 +0900

    create activity_setting.xml

--- app/src/main/java/kr/puze/sanitary/MainActivity.kt
+++ app/src/main/java/kr/puze/sanitary/MainActivity.kt
@@ -2,11 +2,19 @@ package kr.puze.sanitary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    private fun init(){
+        button_add_store_main.setOnClickListener {  }
+        button_setting_main.setOnClickListener {  }
     }
 }
